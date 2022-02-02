c89572cc10fabf6cb0c647e66ee86ab5bfc41668
Bartłomiej Martenka <94762027+BarthazZ@users.noreply.github.com>
2022-02-02 19:48:14 +0100

    Dodaj pliki projektów.

---
+++ Task 6 Conditional Instruction if/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_6_Conditional_Instruction_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A: Ask the user for a number.
+            Console.WriteLine("Please enter some number.");
+
+            // B: Load user entered number.
+            int sn = int.Parse(Console.ReadLine());
+
+            // C: If the user-specified number is greater than or equal to 10, show the + sign.
+            if (sn >= 10)
+            {
+                Console.WriteLine("+");
+            }
+
+            // D: If the number entered by the user is less than 10, show the sign -.
+            if (sn < 10)
+            {
+                Console.WriteLine("-");
+            }
+        }
+    }
+}
